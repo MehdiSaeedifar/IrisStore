df5143decce6ba0f736780a4e6bf642c779627d6
Mehdi <saeedifarmehdi@gmail.com>
2017-06-18 21:55:28 +0430

    auto seed add initialization

--- Iris.Web/Startup.cs
+++ Iris.Web/Startup.cs
@@ -31,7 +31,7 @@ namespace Iris.Web
                       .Use(() => app.GetDataProtectionProvider());
             });
 
-            //IoC.Container.GetInstance<IApplicationUserManager>().SeedDatabase();
+            IoC.Container.GetInstance<IApplicationUserManager>().SeedDatabase();
 
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
